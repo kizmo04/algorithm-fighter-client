c97219a020f7e8ac8623c22629331353e4ac0715
kizmo04 <get.kizmo@gmail.com>
2018-10-20 01:57:43 +0900

    add environment variables

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -26,19 +26,11 @@ import {
   MATCH_PARTNER_GIVE_UP,
   USER_SOCKET_INIT,
 } from '../constants/socketEventTypes';
-// import { config } from '../config';
+import { config } from '../config';
 
-// const { ROOT } = config;
+const { ROOT } = config;
 
-let ROOT;
-
-if (process.env.NODE_ENV === 'development') {
-  ROOT = 'http://api-dev.kizmo04.com';
-} else if (process.env.NODE_ENV === 'production') {
-  ROOT = 'http://api.kizmo04.com';
-}
-
-const socket = io('http://localhost:5000', {
+const socket = io(ROOT, {
   reconnection: true,
   transports: ['websocket', 'polling']
 });
