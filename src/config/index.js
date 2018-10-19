b4c27068c620da6b185877975f82d04ce2aa5411
kizmo04 <get.kizmo@gmail.com>
2018-10-20 01:49:17 +0900

    add environment variables

---
+++ src/config/index.js
@@ -0,0 +1,22 @@
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTO_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+};
+
+let ROOT;
+
+if (process.env.REACT_APP_NODE_ENV === 'development') {
+  ROOT = 'http://api-dev.kizmo04.com';
+} else if (process.env.NODE_ENV === 'production') {
+  ROOT = 'http://api.kizmo04.com';
+}
+
+export const config = {
+  JWT_SECRET: process.env.REACT_APP_JWT_SECRET,
+  firebaseConfig,
+  ROOT
+};
