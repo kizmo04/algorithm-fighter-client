4c1cd4698f74d2be0dc58d92a4b3f48938807f06
kizmo04 <get.kizmo@gmail.com>
2018-10-20 13:58:47 +0900

    Update firebase config

--- src/config/index.js
+++ src/config/index.js
@@ -1,12 +1,3 @@
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTO_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-};
-
 let ROOT;
 
 if (process.env.REACT_APP_NODE_ENV === 'development') {
@@ -17,6 +8,5 @@ if (process.env.REACT_APP_NODE_ENV === 'development') {
 
 export const config = {
   JWT_SECRET: process.env.REACT_APP_JWT_SECRET,
-  firebaseConfig,
   ROOT
 };
--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,7 +1,14 @@
 import firebase from 'firebase';
-import { config } from '../config';
 
-const { firebaseConfig } = config;
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTO_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+};
+
 firebase.initializeApp(firebaseConfig);
 
 const provider = new firebase.auth.GithubAuthProvider();
