a45409e83f76cbb5da34004ea3bd03103142f41e
kizmo04 <get.kizmo@gmail.com>
2018-10-26 16:13:16 +0900

    setup socket ip

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,11 @@
-let ROOT;
+let ROOT, SOCKET_ROOT;
 
 if (process.env.REACT_APP_NODE_ENV === 'development') {
   ROOT = 'https://api-dev.kizmo04.com';
+  SOCKET_ROOT = '';
 } else if (process.env.NODE_ENV === 'production') {
   ROOT = 'https://api.kizmo04.com';
+  SOCKET_ROOT = 'http://172.31.4.127:8081';
 }
 
 export const config = {
